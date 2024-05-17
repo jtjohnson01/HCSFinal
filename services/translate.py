421170a0fb3c0a61ce9164c22cb188672d438d29
jtjohnson01 <jtjohnson01@wm.edu>
2024-05-17 19:47:03 -0400

    Added environment variables to heroku

--- services/translate.py
+++ services/translate.py
@@ -1,9 +1,22 @@
 import os
 from google.cloud import translate_v2 as translate
+from google.oauth2 import service_account
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'key.json'
+credentials_info = {
+    "type": os.getenv("GOOGLE_CLOUD_TYPE"),
+    "project_id": os.getenv("GOOGLE_CLOUD_PROJECT_ID"),
+    "private_key_id": os.getenv("GOOGLE_CLOUD_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("GOOGLE_CLOUD_PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.getenv("GOOGLE_CLOUD_CLIENT_EMAIL"),
+    "client_id": os.getenv("GOOGLE_CLOUD_CLIENT_ID"),
+    "auth_uri": os.getenv("GOOGLE_CLOUD_AUTH_URI"),
+    "token_uri": os.getenv("GOOGLE_CLOUD_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("GOOGLE_CLOUD_AUTH_PROVIDER_CERT_URL"),
+    "client_x509_cert_url": os.getenv("GOOGLE_CLOUD_CLIENT_CERT_URL")
+}
 
-translate_client = translate.Client()
+credentials = service_account.Credentials.from_service_account_info(credentials_info)
+translate_client = translate.Client(credentials=credentials)
 
 def english_to_chinese(word):
     client = translate.Client()
